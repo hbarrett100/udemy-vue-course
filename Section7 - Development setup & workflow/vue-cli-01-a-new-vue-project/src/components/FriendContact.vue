7af530ebd8cc9808062bdba3d05b2c32ff0162ac
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2021-01-31 13:19:53 +0000

    section7 - development setup and workflow

---
+++ Section7 - Development setup & workflow/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <h2> {{ friend.name }} </h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: "conor",
+                name: "conor short",
+                phone: "0861217707",
+                email: "conor@conor.ie",
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+</script>
